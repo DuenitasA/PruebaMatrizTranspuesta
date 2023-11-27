8350f1abef312bacdbf0836205698f9a9225bd4a
Alejandro Duenas <alejandro.duenas@udla.edu.ec>
2023-11-27 16:02:23 -0500

    Primero le pedimos al usuario que ingrese la cantidad de filas y de columnas, ademas se agregan las librerisa de time para poder asignar valores aleatorios a los elementos de la matriz

---
+++ MatrizTranspuesta.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+
+int main(void){
+
+    srand(time(NULL));
+    int a, b;
+
+    printf("Ingrese la cantidad de filas.\n");
+    scanf("%d", &a);
+    printf("Ingrese la cantidad de columnas.\n");
+    scanf("%d", &b);
+
+    int matriz[a][b];
+
+    for (int i = 0; i < a; i++)
+    {
+        for (int j = 0; i < b; j++)
+        {
+            matriz[a]
+        }
+        
+    }
+    
+
+
+    return 0;
+}
